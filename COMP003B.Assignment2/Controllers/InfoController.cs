fdcbb5df64355a2ba0b2608dda3c4fc5307ab58d
Diego Carmona <ultgojifan@gmail.com>
2024-04-04 22:57:35 -0700

    Attempting debuging to find issues

--- COMP003B.Assignment2/Controllers/InfoController.cs
+++ COMP003B.Assignment2/Controllers/InfoController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using COMP003B.Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace COMP003B.Assignment2.Controllers
 {
     public class InfoController : Controller
     {
+        
         public IActionResult About()
         {
             return View();
